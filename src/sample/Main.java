6701bc94c1ae26c74dc0d34aa6aaff2c9c0d81eb
Daniel Litt <Daniel_litt@comcast.net>
2020-09-08 19:28:41 -0400

    Create inital calculator UI
    Added all the necessary buttons and the text
    field to display the output. Changed the name
    fxml file and title of the window.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Binary Calculator");
+        primaryStage.setScene(new Scene(root, 350, 420));
         primaryStage.show();
     }
 
