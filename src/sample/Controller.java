18e77be4d03868b674e75f9fd9b36029881fdcfb
Daniel Litt <Daniel_litt@comcast.net>
2020-09-08 19:42:52 -0400

    Connect 1 and 0 buttons
    The 1 and 0 buttons are connected to functions
    in the controller that allows them to add 1 or
    0 to the calculators display.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField calculatorDisplay;
+
+    public void onePressed(ActionEvent actionEvent) {
+        calculatorDisplay.appendText("1");
+    }
+
+    public void zeroPressed(ActionEvent actionEvent) {
+        calculatorDisplay.appendText("0");
+    }
 }
