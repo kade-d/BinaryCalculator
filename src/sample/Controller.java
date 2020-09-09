a9fa1962bd6845c341356214b5453e926b365d50
Daniel Litt <Daniel_litt@comcast.net>
2020-09-08 20:02:06 -0400

    Limit typed input to 1 and 0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,29 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+
+import java.util.function.UnaryOperator;
 
 public class Controller {
     public TextField calculatorDisplay;
+
+    @FXML
+    public void initialize() {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String text = change.getText();
+
+            if (text.matches("[0-1]*")) {
+                return change;
+            }
+
+            return null;
+        };
+        TextFormatter<String> textFormatter = new TextFormatter<>(filter);
+        calculatorDisplay.setTextFormatter(textFormatter);
+    }
 
     public void onePressed(ActionEvent actionEvent) {
         calculatorDisplay.appendText("1");
