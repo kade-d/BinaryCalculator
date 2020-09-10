9cf4d7ef17c0a57dfc11322548523b1f0a940de0
Daniel Litt <Daniel_litt@comcast.net>
2020-09-10 17:04:07 -0400

    Make BinaryConverter work with negative numbers

--- src/sample/util/BinaryConverter.java
+++ src/sample/util/BinaryConverter.java
@@ -3,11 +3,19 @@ package sample.util;
 public class BinaryConverter {
 
     public static String convertBinaryToDecimal(String binaryNumber){
+        if (binaryNumber.startsWith("-")){
+            int decimalNumber = Integer.parseInt(binaryNumber.substring(1), 2);
+            return Integer.toString(-decimalNumber);
+        }
         int decimalNumber = Integer.parseInt(binaryNumber, 2);
         return Integer.toString(decimalNumber);
     }
 
     public static String convertDecimalToBinary(String decimalNumber){
+        if (decimalNumber.startsWith("-")){
+            int parsedNum = Integer.parseInt(decimalNumber.substring(1));
+            return "-"+Integer.toBinaryString(parsedNum);
+        }
         int parsedNum = Integer.parseInt(decimalNumber);
         return Integer.toBinaryString(parsedNum);
     }
